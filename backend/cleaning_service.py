e5766e9cf819b8032764a7e689e04fbfabd069c2
Bian <kadekbian1717@gmail.com>
2026-01-15 22:37:08 +0700

    refactor: update the structure to monolith

--- backend/cleaning_service.py
+++ backend/cleaning_service.py
@@ -1,15 +1,9 @@
 import pandas as pd
 import io
 
-class CsvCleaner:
+def clean(file_path):
+    df = pd.read_csv(io.BytesIO(file_path))
+    df.dropna(axis=0, inplace=True)
+    df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
 
-    @staticmethod
-    def clean(file_path):
-        df = pd.read_csv(file_path)
-        df.dropna(axis=0, inplace=True)
-        df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
-
-        stream = io.StringIO()
-        df.to_csv(stream, index=False)
-        stream.seek(0)
-        return stream
+    return df.to_csv(index=False)
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,62 +1,41 @@
 import streamlit as st
-import requests
-import re
+import sys
 import os
+from datetime import datetime
 
-# 1. Konfigurasi Halaman (Biar terlihat profesional)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from backend.cleaning_service import clean
+
+
 st.set_page_config(page_title="CSV_Cleaner", page_icon="👁️")
 
-# 2. Judul dan Deskripsi
+
 st.title("CSV Cleaner Tools")
 st.write("Upload CSV file to be cleaned")
 
-# 3. Widget Upload Gambar
+
 uploaded_file = st.file_uploader("Select CSV file...", type=["csv"])
 
 
-def get_filename_from_response(response, default="result.csv"):
-    content_disposition = response.headers.get("content-disposition", "")
-    
-    match = re.search(r'filename="?([^"]+)"?', content_disposition)
-    if match:
-        return match.group(1)
-    
-    return default
 
-# 4. Logika Utama
 if uploaded_file is not None:
     
-    # Tombol Prediksi
+
     if st.button("Clean now!"):
         with st.spinner('Processing file...'):
             try:
-                # --- CARA KONEK KE API ---
-                # Siapkan file untuk dikirim
-                # "file" adalah nama parameter yang ditunggu oleh FastAPI nanti
-                files = {"file": uploaded_file.getvalue()}
-                
-                # Tembak ke API (Asumsi jalan di localhost:8000)
-                api_url = os.getenv("API_URL", "http://localhost:8000/upload-csv")
-                response = requests.post(api_url, files=files)
-    
-                # Cek status respon
-                if response.status_code == 200:
-                    filename = get_filename_from_response(response)
 
-                    st.success("Done!")
-                    st.download_button(
-                        label="⬇️ Download hasil CSV",
-                        data=response.content,
-                        file_name=filename,
-                        mime="text/csv"
+                bytes_data = uploaded_file.getvalue()
+                result_csv = clean(bytes_data)
 
-                    )
-                    
-                else:
-                    st.error(f"Error API: {response.status_code}")
+                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                name_file = f"cleaned_data_{timestamp}.csv"
+
+                st.success("Done!")
+                st.download_button("Download!", result_csv, name_file)
                     
             except Exception as e:
-                st.error(f"Gagal konek ke server. Pastikan API nyala! Error: {e}")
+                st.error(f"Error: {e}")
 
 # Sidebar (Pemanis)
 st.sidebar.title("About")
